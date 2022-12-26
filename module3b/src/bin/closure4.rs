15a10a5bd1c92f464a7c39355dd122db3fbb185d
raghav-rama <hackerer528@gmail.com>
2022-12-26 16:58:00 +0530

    feat: solve closure4.rs

--- module3b/src/bin/closure4.rs
+++ module3b/src/bin/closure4.rs
@@ -8,6 +8,34 @@
 // * Use the map function to create the User
 // * Print out the User struct if found, or a "not found" message if not
 
+use std::io;
+
 fn main() {
-    
+    println!("Enter name: ");
+    let mut buf = String::new();
+    io::stdin()
+        .read_line(&mut buf)
+        .expect("problem reading stdin");
+    buf = buf.trim().to_string();
+    let res: Option<User> = find_user(buf.as_str()).map(|user_id| User { user_id, name: buf });
+    match res {
+        Some(data) => println!("{:?}", data),
+        None => println!("Not found"),
+    }
+}
+
+#[derive(Debug)]
+struct User {
+    user_id: i32,
+    name: String,
+}
+
+fn find_user(name: &str) -> Option<i32> {
+    let name = name.to_lowercase();
+    match name.as_str() {
+        "trizwit" => Some(1),
+        "prathamesh" => Some(3),
+        "nagi" => Some(7),
+        _ => None,
+    }
 }
